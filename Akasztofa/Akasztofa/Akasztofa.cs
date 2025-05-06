37fdfd24b0807761e5720b6b4ceb8176a2aebbfe
Sebastian Varga <xAzta@users.noreply.github.com>
2025-05-06 19:59:31 +0200

    Add files via upload

---
+++ Akasztofa/Akasztofa/Akasztofa.cs
@@ -0,0 +1,52 @@
+﻿string[] szavak= { "törpe", "pálinka", "bor", "sör", "nyuszi", "cica" };
+Random rnd = new Random();
+
+Console.SetCursorPosition(10, 0);
+Console.WriteLine("Akasztófa");
+
+Char[] kitalalando = szavak[rnd.Next(0,szavak.Length)].ToCharArray();
+Char[] kitalalt = new char[kitalalando.Length];
+for (int i = 0; i < kitalalt.Length; i++) kitalalt[i] = '*';
+
+int maxTipp = kitalalando.Length + 2;
+int tippSzam = 0;
+
+ConsoleKeyInfo ujJatek;
+
+List<char> chars = new List<char>();
+do
+{
+    Console.SetCursorPosition(1, 2);
+    Console.WriteLine(string.Join("", kitalalt));
+    Console.SetCursorPosition(1, 4);
+    if (chars.Count != 0 ) Console.Write($"Eddigi tippek: {String.Join(", ",chars)}");
+    Console.SetCursorPosition(1, 5);
+    Console.Write("Tippelj egy betűt: ");
+    var tipp = Console.ReadKey().KeyChar;
+    chars.Add(tipp);
+    tippSzam++;
+    char[] ujKitalalt = new char[kitalalt.Length];
+    for (int i = 0; i < kitalalando.Length; i++)
+    {
+        if (kitalalando[i] == tipp)
+        {
+            kitalalt[i] = tipp;
+        }
+    }
+}
+while ((tippSzam <= maxTipp) && (string.Join("", kitalalando) != string.Join("", kitalalt)));
+
+Console.SetCursorPosition(1, 2);
+Console.WriteLine(string.Join("", kitalalando));
+
+Console.SetCursorPosition(1, 7);
+
+if (String.Join("", kitalalando) == string.Join("", kitalalt))
+    Console.WriteLine("Kitaláltad! Ügyes vagy!");
+else
+    Console.WriteLine("Sajnos ez most nem sikerült!");
+
+    Console.WriteLine("Még egy játék (i/n)?");
+    ujJatek = Console.ReadKey(true);
+
+} while (ujJatek.Key != ConsoleKey.N);
---
+++ Szoveg_valtozok/Szoveg_valtozok/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string szoveg1 = "Almafa";
+string szoveg2 = "Almafa";
+
+if (szoveg1 == szoveg2)
+    Console.WriteLine("Egyenlőek");
+
+int[] t1 = { 1, 2, 3 };
+int[] t2 = { 1, 2, 3 };
+
+if (t1 == t2)
+    Console.WriteLine("Tömbök egyenlőel");
+else Console.WriteLine("A tömbök nem egyenlőek");
+
+string s1 = "";
+s1 = null;
+s1 = "SzoftI/K";
+s1 = s1 + s1;
+s1 += " Cica";
+Console.WriteLine(s1);
+Console.WriteLine(s1.Length);
+Console.WriteLine(s1[20]);
+//s1[20] = '&';
+s1 = "0123456789";
+Console.WriteLine(s1.Remove(3,2));
+Console.WriteLine(s1.Substring(3,2));
+s1 = s1.Insert(3,"\talma\n".TrimEnd([ 'a','\n'] ) ) ;
+Console.WriteLine(s1.ToUpper());
+Console.WriteLine(s1);
+Console.WriteLine(s1.IndexOf("ala"));
+
+string sor = "Ez egy sor egy verből";
+var szavak = sor.Split(' ');
+
+Console.WriteLine(szavak.ToString());
+
+for (int i = 0; i <szavak.Length; i++)
+{
+    Console.Write(szavak[i] + "; ");
+}
+Console.WriteLine();
+
+foreach (var szo in szavak) Console.Write(szo + ", ");
+
+Console.WriteLine(string.Join(": ", szavak));
+
+var charTomb = s1.ToCharArray();
+charTomb[0] = 'B';
+Console.WriteLine("CharTömb: "+String.Join("",charTomb));
